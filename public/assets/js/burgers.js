2dac65c9947240dff02edf80102ef5b65154f34a
moskowitza <moskowitzaaron@mac.com>
2018-05-07 17:21:25 -0400

    handlebar helper?

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,7 +2,7 @@
 $(function() {
     $(".change-devoured").on("click", function(event) {
       var id = $(this).data("id");
-      var newEat = $(this).data("neweat");
+      var newEat = $(this).data("neweaten");
   
       var newDevouredState = {
         devoured: newEat
