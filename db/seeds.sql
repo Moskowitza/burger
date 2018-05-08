de04bb8aa3dcbf51d4f9eff10951c795b2e8e2c1
moskowitza <moskowitzaaron@mac.com>
2018-05-08 08:39:09 -0400

    catching up

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,5 +2,5 @@ INSERT INTO burgers (name,devoured) VALUES ('Beyond Burger',false);
 INSERT INTO burgers (name,devoured) VALUES ('Gardien Beefless Burger',true);
 INSERT INTO burgers (name,devoured) VALUES ('Boca Burger',false);
 INSERT INTO burgers (name,devoured) VALUES ('Beast Burger',false);
-INSERT INTO burgers (name,devoured) VALUES ('Portabello Burger',false);
+INSERT INTO burgers (name,devoured) VALUES ('Portabello Burger',true);
 INSERT INTO burgers (name,devoured) VALUES ('Field Roast Burger',false);
